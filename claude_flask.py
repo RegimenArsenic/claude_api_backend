6aaaed8c3344bac9b5f7095131347b04d8b6378b
RegimenArsenic <pio71ctt@gmail.com>
2023-11-26 00:20:19 +0800

    updatte

--- claude_flask.py
+++ claude_flask.py
@@ -4,142 +4,192 @@ import sys
 sys.path.append("./Claude-API/claude-api")
 import os
 import json
-from flask import Flask, request, jsonify, stream_with_context, Response
+from flask import Flask, request, jsonify, Response
 from claude_api import Client
+import datetime
 
 app = Flask(__name__)
 
 
-@app.route('/chat', methods=['POST'])
+# 注册中间件，使其对所有请求生效
+@app.before_request
+def before_request():
+  result = authorize_handler(request)
+  if not result:
+    return {'message': 'Unauthorized'}, 401
+
+
+@app.route('/login', methods=['POST'])
+def login_handler():
+  # try:
+  data = request.get_json()
+  if data:
+    if data.get('password') == md5_hash(os.getenv('login_password')):
+      iat = datetime.datetime.utcnow()
+      payload = {
+          'iss': 'momo',
+          'iat': iat.timestamp(),  # 开始时间
+          'exp': (iat + datetime.timedelta(days=0, hours=int(os.getenv('expires_in')))).timestamp(),  # 过期时间
+          'data': {
+              'id': data.get('id'),
+              'login_time': iat.timestamp()
+          },
+      }
+      return jsonify({
+          'error': 0,
+          'data': '登录成功',
+          'id': data.get('id'),
+          'token': sign(payload),
+      })
+    else:
+      return {'message': '密码错误'}, 401
+  return {'message': '发生内部错误'}, 401
+
+
+# except Exception as err:
+#   print(str(err))
+#   return {'message': '发生内部错误'}, 500
+
+
+@app.route('/api/chat', methods=['POST'])
 def create_chat():
-    data = request.get_json()
-    prompt = data['prompt']
+  data = request.get_json()
+  prompt = data['prompt']
 
-    cookie = get_cookie()
-    client = Client(cookie)
-    conversation = client.create_new_chat()
-    conversation_id = conversation['uuid']
+  cookie = get_cookie()
+  client = Client(cookie)
+  conversation = client.create_new_chat()
+  conversation_id = conversation['uuid']
 
-    response = client.send_message(prompt, conversation_id)
-    return jsonify({'conversation_id': conversation_id, 'response': response})
+  response = client.send_message(prompt, conversation_id)
+  return jsonify({'conversation_id': conversation_id, 'response': response})
 
 
-@app.route('/chat/<conversation_id>')
+@app.route('/api/chat/<conversation_id>')
 def get_chat_history(conversation_id):
-    cookie = get_cookie()
-    client = Client(cookie)
-    history = client.chat_conversation_history(conversation_id)
-    return jsonify(history)
+  cookie = get_cookie()
+  client = Client(cookie)
+  history = client.chat_conversation_history(conversation_id)
+  return jsonify(history)
 
 
-@app.route('/send', methods=['POST'])
+@app.route('/api/send', methods=['POST'])
 def send_message():
-    file = None
-    if (len(request.files)):
-        file = request.files['file']
-        data = json.loads(request.form.get("json"))
-    else:
-        data = request.get_json()
-    conversation_id = data['conversation_id']
-    prompt = data['prompt']
-    file_path = None
-    if file:
-        file_path = save_upload_file(file)
-    cookie = get_cookie()
-    client = Client(cookie)
-    response = client.send_message(prompt, conversation_id, file_path)
-    return jsonify({'response': response})
-
-
-@app.route('/stream', methods=['POST'])
-def query_stream():
-    file = None
-    if (len(request.files)):
-        file = request.files['file']
-        data = json.loads(request.form.get("json"))
-    else:
-        data = request.get_json()
-    conversation_id = data['conversation_id']
-    prompt = data['prompt']
-    file_path = None
-    if file:
-        file_path = save_upload_file(file)
-    cookie = get_cookie()
-    client = Client(cookie)
-
-    def handle_result(prompt, conversation_id, file_path):
-      for data in client._query_stream(prompt, conversation_id, file_path):
-        yield f"data: {data}\n\n"
-
-    return Response(handle_result(prompt, conversation_id, file_path), content_type='text/event-stream')
-
-
-@app.route('/reset', methods=['POST'])
-def reset_conversations():
-    cookie = get_cookie()
-    client = Client(cookie)
-    result = client.reset_all()
-    return jsonify({'result': result})
-
-
-@app.route('/rename', methods=['POST'])
-def rename_conversation():
-    data = request.get_json()
-    conversation_id = data['conversation_id']
-    title = data['title']
-
-    cookie = get_cookie()
-    client = Client(cookie)
-    result = client.rename_chat(title, conversation_id)
-    return jsonify({'result': result})
-
-
-@app.route('/upload', methods=['POST'])
-def upload_attachment():
+  file = None
+  if (len(request.files)):
     file = request.files['file']
-    if file:
-        file_path = save_upload_file(file)
-        cookie = get_cookie()
-        client = Client(cookie)
-        response = client.upload_attachment(file_path)
-        return jsonify({'result': response})
-    else:
-        return jsonify({'error': 'No file uploaded'}), 400
+    data = json.loads(request.form.get("json"))
+  else:
+    data = request.get_json()
+  conversation_id = data['conversation_id']
+  prompt = data['prompt']
+  file_path = None
+  if file:
+    file_path = save_upload_file(file)
+  cookie = get_cookie()
+  client = Client(cookie)
+  response = client.send_message(prompt, conversation_id, file_path)
+  return jsonify({'response': response})
+
+
+@app.route('/api/stream', methods=['POST'])
+def query_stream():
+  file = None
+  if (len(request.files)):
+    file = request.files['file']
+    data = json.loads(request.form.get("json"))
+  else:
+    data = request.get_json()
+  conversation_id = data['conversation_id']
+  prompt = data['prompt']
+  file_path = None
+  if file:
+    file_path = save_upload_file(file)
+  cookie = get_cookie()
+  client = Client(cookie)
+
+  def handle_result(prompt, conversation_id, file_path):
+    for data in client._query_stream(prompt, conversation_id, file_path):
+      yield f"event:update\n\ndata: '{data}'\n\n"
+    yield f"event:finish\n\ndata: ''\n\n"
+
+  return Response(handle_result(prompt, conversation_id, file_path), content_type='text/event-stream')
+
+
+@app.route('/api/reset', methods=['POST'])
+def reset_conversations():
+  cookie = get_cookie()
+  client = Client(cookie)
+  result = client.reset_all()
+  return jsonify({'result': result})
+
+
+@app.route('/api/rename', methods=['POST'])
+def rename_conversation():
+  data = request.get_json()
+  conversation_id = data['conversation_id']
+  title = data['title']
+
+  cookie = get_cookie()
+  client = Client(cookie)
+  result = client.rename_chat(title, conversation_id)
+  return jsonify({'result': result})
+
+
+@app.route('/api/upload', methods=['POST'])
+def upload_attachment():
+  file = request.files['file']
+  if file:
+    file_path = save_upload_file(file)
+    cookie = get_cookie()
+    client = Client(cookie)
+    response = client.upload_attachment(file_path)
+    return jsonify({'result': response})
+  else:
+    return jsonify({'error': 'No file uploaded'}), 400
 
 
 def save_upload_file(file):
-    uploads_dir = os.getenv('uploads')
-    file_path = os.path.join(uploads_dir, file.filename)
-    file.save(file_path)
-    return file_path
+  uploads_dir = os.getenv('uploads')
+  file_path = os.path.join(uploads_dir, file.filename)
+  file.save(file_path)
+  return file_path
 
 
-@app.route('/conversations')
+@app.route('/api/conversations')
 def list_all_conversations():
-    cookie = get_cookie()
-    client = Client(cookie)
-    conversations = client.list_all_conversations()
-    return jsonify(conversations)
+  cookie = get_cookie()
+  client = Client(cookie)
+  conversations = client.list_all_conversations()
+  return jsonify(conversations)
 
 
-@app.route('/history/<conversation_id>')
+@app.route('/api/history/<conversation_id>')
 def chat_conversation_history(conversation_id):
-    cookie = get_cookie()
-    client = Client(cookie)
-    history = client.chat_conversation_history(conversation_id)
-    return jsonify(history)
+  cookie = get_cookie()
+  client = Client(cookie)
+  history = client.chat_conversation_history(conversation_id)
+  return jsonify(history)
 
 
-@app.route('/setcookie', methods=['POST'])
+@app.route('/api/delete/<conversation_id>')
+def chat_delete_conversation(conversation_id):
+  cookie = get_cookie()
+  client = Client(cookie)
+  result = client.delete_conversation(conversation_id)
+  return jsonify({result: result})
+
+
+@app.route('/api/setcookie', methods=['POST'])
 def setcookie():
-    data = request.get_json()
-    cookie = data['cookie']
-    if cookie:
-        return jsonify({'result': set_cookie(cookie)})
-    else:
-        return jsonify({'error': 'Invaild cookie environment variable'}), 400
+  data = request.get_json()
+  cookie = data['cookie']
+  if cookie:
+    return jsonify({'result': set_cookie(cookie)})
+  else:
+    return jsonify({'error': 'Invaild cookie environment variable'}), 400
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4149, debug=True, use_reloader=True)
-    app.default_encoding = 'utf-8'
+  app.run(host='0.0.0.0', port=4149, debug=True, use_reloader=True)
+  app.default_encoding = 'utf-8'
--- common/utils.py
+++ common/utils.py
@@ -1,74 +1,146 @@
 import io
 import os
+import jwt
+import hashlib
 from dotenv import load_dotenv
 from PIL import Image
 
 
-def fsize(file):
-    if isinstance(file, io.BytesIO):
-        return file.getbuffer().nbytes
-    elif isinstance(file, str):
-        return os.path.getsize(file)
-    elif hasattr(file, "seek") and hasattr(file, "tell"):
-        pos = file.tell()
-        file.seek(0, os.SEEK_END)
-        size = file.tell()
-        file.seek(pos)
-        return size
+def md5_hash(input_string):
+  md5 = hashlib.md5()
+  md5.update(input_string.encode('utf-8'))
+  hashed_string = md5.hexdigest()
+  return hashed_string
+
+
+# 签名函数
+def sign(data={}):
+  return jwt.encode(data, os.getenv('secret_key'), algorithm='HS256')
+
+
+@staticmethod
+def decode_auth_token(auth_token):
+  try:
+    payload = jwt.decode(auth_token, os.getenv('secret_key'), algorithms='HS256')
+    if ('data' in payload and 'id' in payload['data']):
+      return payload
     else:
-        raise TypeError("Unsupported type")
+      raise jwt.InvalidTokenError
+  except jwt.ExpiredSignatureError:
+    return 'Token过期'
+  except jwt.InvalidTokenError:
+    return '无效Token'
+
+
+# 用户鉴权
+def authorize(req):
+  try:
+    data = req.get_json()
+    authPassword = os.getenv('login_password')
+    if (not authPassword):
+      return True
+    if (req is None):
+      return False
+    authorization = req.cookies.get('Authorization')
+    if (authorization):
+      payload = decode_auth_token(authorization)
+      if not isinstance(payload, str):
+        authPassword = hashlib.md5(authPassword).hexdigest()
+        password = data.get('password')
+        if (password != authPassword):
+          return False
+        else:
+          return True
+    return False
+
+  except jwt.ExpiredSignatureError:
+    #result = 'Token已更改，请重新登录获取'
+    return False
+
+  except jwt.InvalidTokenError:
+    #result = '没有提供认证token'
+    return False
+
+
+# 定义白名单
+white_list = ['/login']
+
+
+# 授权处理函数
+async def authorize_handler(req):
+  if req.url not in white_list:
+    # 判断请求头是否携带正确的token
+    try:
+      return authorize(req)
+    except Exception as e:
+      print(e)
+      return False
+  else:
+    return True
+
+
+def fsize(file):
+  if isinstance(file, io.BytesIO):
+    return file.getbuffer().nbytes
+  elif isinstance(file, str):
+    return os.path.getsize(file)
+  elif hasattr(file, "seek") and hasattr(file, "tell"):
+    pos = file.tell()
+    file.seek(0, os.SEEK_END)
+    size = file.tell()
+    file.seek(pos)
+    return size
+  else:
+    raise TypeError("Unsupported type")
 
 
 def compress_imgfile(file, max_size):
-    if fsize(file) <= max_size:
-        return file
-    file.seek(0)
-    img = Image.open(file)
-    rgb_image = img.convert("RGB")
-    quality = 95
-    while True:
-        out_buf = io.BytesIO()
-        rgb_image.save(out_buf, "JPEG", quality=quality)
-        if fsize(out_buf) <= max_size:
-            return out_buf
-        quality -= 5
+  if fsize(file) <= max_size:
+    return file
+  file.seek(0)
+  img = Image.open(file)
+  rgb_image = img.convert("RGB")
+  quality = 95
+  while True:
+    out_buf = io.BytesIO()
+    rgb_image.save(out_buf, "JPEG", quality=quality)
+    if fsize(out_buf) <= max_size:
+      return out_buf
+    quality -= 5
 
 
 def split_string_by_utf8_length(string, max_length, max_split=0):
-    encoded = string.encode("utf-8")
-    start, end = 0, 0
-    result = []
-    while end < len(encoded):
-        if max_split > 0 and len(result) >= max_split:
-            result.append(encoded[start:].decode("utf-8"))
-            break
-        end = min(start + max_length, len(encoded))
-        # 如果当前字节不是 UTF-8 编码的开始字节，则向前查找直到找到开始字节为止
-        while end < len(encoded) and (encoded[end] & 0b11000000) == 0b10000000:
-            end -= 1
-        result.append(encoded[start:end].decode("utf-8"))
-        start = end
-    return result
+  encoded = string.encode("utf-8")
+  start, end = 0, 0
+  result = []
+  while end < len(encoded):
+    if max_split > 0 and len(result) >= max_split:
+      result.append(encoded[start:].decode("utf-8"))
+      break
+    end = min(start + max_length, len(encoded))
+    # 如果当前字节不是 UTF-8 编码的开始字节，则向前查找直到找到开始字节为止
+    while end < len(encoded) and (encoded[end] & 0b11000000) == 0b10000000:
+      end -= 1
+    result.append(encoded[start:end].decode("utf-8"))
+    start = end
+  return result
+
 
 load_dotenv()
+
+
 def get_cookie():
-    #cookie = os.environ.get('cookie')
-    cookie = os.getenv('cookie')
-    print(cookie)
-    if not cookie:
-        raise ValueError("Please set the 'cookie' environment variable.")
-    return cookie
-def  get_proxy()  -> bool:
-    #cookie = os.environ.get('cookie')
-    isproxy = os.getenv('ISPROXY')
-    print(isproxy)
-    if not isproxy:
-        return False
-    else:
-        return True if isproxy.lower() == 'true' else False
+  #cookie = os.environ.get('cookie')
+  cookie = os.getenv('cookie')
+  print(cookie)
+  if not cookie:
+    raise ValueError("Please set the 'cookie' environment variable.")
+  return cookie
+
+
 def set_cookie(cookie):
-    if not cookie:
-        raise ValueError("Invaild 'cookie' environment variable.")
-    os.environ["cookie"]=cookie
-    print(os.getenv('cookie'))
-    return cookie
+  if not cookie:
+    raise ValueError("Invaild 'cookie' environment variable.")
+  os.environ["cookie"] = cookie
+  print(os.getenv('cookie'))
+  return cookie
